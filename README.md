aa70e88d5b362054df5de9f55fc6e7db9d5440d4
shubham <rautelashubham@gmail.com>
2021-07-20 22:15:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# matrix
+		Discord Bot generated with [create-discord-bot](https://github.com/HZooly/create-discord-bot)
+
+		To get the Bot invitation link, you can use [discord-bot-invitation](https://github.com/HZooly/discord-bot-invitation)
+	# discord-bot
---
+++ bot.js
@@ -0,0 +1,7 @@
+/* matrix generated with create-discord-bot CLI */
+    const Discord = require('discord.js')
+	const client = new Discord.Client()
+    client.on('ready', () => {
+	    console.log('Bot is ready!')
+    })
+    client.login('ODY3MDc5NjIyOTIyOTkzNzE4.YPb40w.rBbXewAY3RF7mNz09HDpHf91W4E');
